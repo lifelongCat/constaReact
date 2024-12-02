a16234582bb401875724b9b049ac0b200705d58f
lifelongCat <lifelongcat@yandex.ru>
2024-12-02 14:48:09 +0300

    feat: add filling user data in redux after page reloading (#8)

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,22 +1,39 @@
 import { Button } from '@consta/uikit/Button';
 import PagesLinks from "./PagesLinks.tsx";
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
-import { clearUser } from "../../store/reducers/userReducer.ts";
+import { setUser, clearUser } from "../../store/reducers/userReducer.ts";
 import { useNavigate } from "react-router-dom";
 import { RootState } from "../../store/store.tsx";
 
 
 const Header = () => {
     const user = useSelector((state: RootState) => state.user);
+    const token = localStorage.getItem("token");
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    const decodeToken = (token: string) => {
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+        return JSON.parse(jsonPayload);
+    }
+
+    useEffect(() => {
+        if (!token) return;
+        const tokenPayload = decodeToken(token);
+        dispatch(setUser({accessToken: token, ...tokenPayload}));
+    }, [dispatch, token])
 
     return (
         <header className="footer-header-block">
             <PagesLinks />
             <div className="footer-header-part">
                 {
-                    user.token
+                    token
                     ? <Button
                             view="clear"
                             label={user.username}
@@ -26,7 +43,7 @@ const Header = () => {
                     : null
                 }
                 {
-                    user.token
+                    token
                     ? <Button
                             view="clear"
                             label="Выход"
