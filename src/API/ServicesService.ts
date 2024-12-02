c1a12a9b9c30af4aa00b2dc553dd0257f4db0d06
lifelongCat <lifelongcat@yandex.ru>
2024-12-03 01:34:14 +0300

    feat: create concrete service page (#9)

--- src/API/ServicesService.ts
+++ src/API/ServicesService.ts
@@ -4,4 +4,10 @@ export default class ServicesService {
             "https://673423afa042ab85d1190055.mockapi.io/api/v1/services"
         ).then(response => response.json());
     }
+
+    static async getByID(id: number) {
+        return await fetch(
+            `https://673423afa042ab85d1190055.mockapi.io/api/v1/services/${id}`
+        ).then(response => response.json());
+    }
 }
