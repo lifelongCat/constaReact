181dcb2531291fb5ffe1eb7228c71c76a1789698
lifelongCat <lifelongcat@yandex.ru>
2024-12-03 13:41:05 +0300

    feat: add go back button on 404 page (#16)

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,8 +1,20 @@
 import { Responses404 } from '@consta/uikit/Responses404';
+import { Button } from "@consta/uikit/Button";
+import { useNavigate } from 'react-router-dom';
 
 
 const NotFound = () => {
-    return <Responses404 />
+    const navigate = useNavigate();
+
+    return (
+        <Responses404
+            actions={<Button
+                view="ghost"
+                label="Вернуться назад"
+                onClick={() => navigate(-1)}
+            />}
+        />
+    )
 };
 
 export default NotFound;
